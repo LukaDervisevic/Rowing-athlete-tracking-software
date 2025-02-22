422488a29c1655631b1506859e2a1464e8549fe9
LukaDervisevic <lukadervisevic@gmail.com>
2025-02-22 19:25:34 +0100

    kreiranje dizajna forme drzave

--- Client/src/main/java/forme/drzava/DrzavaForma.java
+++ Client/src/main/java/forme/drzava/DrzavaForma.java
@@ -44,21 +44,138 @@ public class DrzavaForma extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        glavnaFormaTable1 = new forme.utils.GlavnaFormaTable();
+        dodajDrzavuBtn = new javax.swing.JButton();
+        obrisiDrzavuBtn = new javax.swing.JButton();
+        adresaInput = new javax.swing.JTextField();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 737, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout());
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel2.setFont(new java.awt.Font("JetBrains Mono NL", 1, 24)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Unesite naziv države");
+
+        jLabel3.setFont(new java.awt.Font("JetBrains Mono", 3, 18)); // NOI18N
+        jLabel3.setText("Naziv države:");
+
+        glavnaFormaTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(glavnaFormaTable1);
+
+        dodajDrzavuBtn.setBackground(new java.awt.Color(13, 146, 244));
+        dodajDrzavuBtn.setFont(new java.awt.Font("JetBrains Mono", 1, 19)); // NOI18N
+        dodajDrzavuBtn.setForeground(new java.awt.Color(255, 255, 255));
+        dodajDrzavuBtn.setText("Dodaj");
+        dodajDrzavuBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dodajDrzavuBtnActionPerformed(evt);
+            }
+        });
+
+        obrisiDrzavuBtn.setBackground(new java.awt.Color(13, 146, 244));
+        obrisiDrzavuBtn.setFont(new java.awt.Font("JetBrains Mono", 1, 19)); // NOI18N
+        obrisiDrzavuBtn.setForeground(new java.awt.Color(255, 255, 255));
+        obrisiDrzavuBtn.setText("Obriši");
+        obrisiDrzavuBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                obrisiDrzavuBtnActionPerformed(evt);
+            }
+        });
+
+        adresaInput.setFont(new java.awt.Font("JetBrains Mono", 2, 18)); // NOI18N
+        adresaInput.setForeground(new java.awt.Color(153, 153, 153));
+        adresaInput.setText("Unesite državu...");
+        adresaInput.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(153, 153, 153)));
+        adresaInput.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                adresaInputFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                adresaInputFocusLost(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(adresaInput))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addComponent(obrisiDrzavuBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(dodajDrzavuBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 376, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(43, 43, 43))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 387, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(adresaInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(dodajDrzavuBtn)
+                    .addComponent(obrisiDrzavuBtn))
+                .addGap(14, 14, 14))
         );
+
+        getContentPane().add(jPanel2);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void dodajDrzavuBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dodajDrzavuBtnActionPerformed
+        // TODO add your handling code here:
+
+        
+    }//GEN-LAST:event_dodajDrzavuBtnActionPerformed
+
+    private void obrisiDrzavuBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_obrisiDrzavuBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_obrisiDrzavuBtnActionPerformed
+
+    private void adresaInputFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_adresaInputFocusGained
+        if (adresaInput.getText().equals("Unesite adresu...")) {
+            adresaInput.setText("");
+        }
+    }//GEN-LAST:event_adresaInputFocusGained
+
+    private void adresaInputFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_adresaInputFocusLost
+        // TODO add your handling code here:
+        if (adresaInput.getText().isEmpty()) {
+            adresaInput.setText("Unesite adresu...");
+        }
+    }//GEN-LAST:event_adresaInputFocusLost
 
     /**
      * @param args the command line arguments
@@ -103,5 +220,13 @@ public class DrzavaForma extends javax.swing.JDialog {
 //    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField adresaInput;
+    private javax.swing.JButton dodajDrzavuBtn;
+    private forme.utils.GlavnaFormaTable glavnaFormaTable1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton obrisiDrzavuBtn;
     // End of variables declaration//GEN-END:variables
 }
